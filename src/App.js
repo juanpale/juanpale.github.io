7a2477fab118af2e9ff877eda9a93cce012bb6d9
juanpale <juanpale@users.noreply.github.com>
2019-03-23 12:08:06 -0300

    init-webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import image from "./image.jpg";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="vertical">
+          <div
+            // className={styles.bannerContainer}
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center"
+            }}
           >
-            Learn React
-          </a>
-        </header>
+            <ParallaxBanner
+              // className={styles.bannerBg}
+              layers={[
+                {
+                  image:
+                    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/105988/banner.jpg",
+                  amount: 0.2
+                }
+              ]}
+            >
+              <div
+                // className={styles.parallaxChildren}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  display: "flex",
+                  flexFow: "row wrap",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+              >
+                <h1>Headline Text</h1>
+              </div>
+            </ParallaxBanner>
+          </div>
+        </div>
       </div>
     );
   }
