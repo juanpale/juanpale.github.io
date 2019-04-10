bfbc97895163b7239e54cab3f0d953b20073e3d7
juanpale <juanpale@users.noreply.github.com>
2019-04-10 00:30:28 -0300

    change text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -37,7 +37,7 @@ export default class Home extends Component {
           <TextContainer>
             I'm a computer engineer, passionate about understanding and solving
             problems, to improve new business and existence ones. <br />
-            Build a solution is process where several interests should be
+            Build a solution is a process where several interests should be
             aligned, because of that I believe in the importance of teamwork,
             taking the other's opinions to create a better result.
           </TextContainer>
