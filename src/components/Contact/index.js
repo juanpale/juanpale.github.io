d4990c470edd1c78c95bf39f21d592cb246e46b3
juanpale <juanpale@users.noreply.github.com>
2019-11-14 21:04:19 -0300

    new link to medium

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import contact from "assets/images/contact.jpg";
 import MailIcon from "assets/icons/MailIcon";
 import GithubIcon from "assets/icons/GithubIcon";
 import LinkedinIcon from "assets/icons/LinkedinIcon";
+import MediumIcon from "assets/icons/MediumIcon";
 import { Wrapper, IconWrapper, TextContainer } from "./styles";
 import { ParallaxBanner } from "react-scroll-parallax";
 
@@ -24,7 +25,10 @@ export default class Home extends Component {
         ]}
       >
         <Wrapper>
-          <IconWrapper href="mailto:juanlemoswork@gmail.com?Subject=Hi%20Juan!">
+          <IconWrapper
+            href="mailto:juanlemoswork@gmail.com?Subject=Hi%20Juan!"
+            style={{ marginTop: "70px" }}
+          >
             <MailIcon color={"white"} size={80} />
             <TextContainer>{"juanlemoswork@gmail.com"}</TextContainer>
           </IconWrapper>
@@ -35,6 +39,10 @@ export default class Home extends Component {
           <IconWrapper href="https://github.com/juanpale">
             <GithubIcon color={"white"} size={90} />
             <TextContainer>{"github.com/juanpale"}</TextContainer>
+          </IconWrapper>
+          <IconWrapper href="https://medium.com/@juanlemos">
+            <MediumIcon color={"white"} size={90} />
+            <TextContainer>{"medium.com/@juanlemos"}</TextContainer>
           </IconWrapper>
         </Wrapper>
       </ParallaxBanner>
